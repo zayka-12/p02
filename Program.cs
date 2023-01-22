76f18b1abc421905a7f1999aa50bd1a32c1cea8e
zayka <120934899+zayka-12@users.noreply.github.com>
2023-01-22 15:13:26 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+int Prompt(string msg)
+{
+    System.Console.WriteLine($"{msg} : ");
+    int res = Convert.ToInt32(Console.ReadLine());
+    return res;
+}
+int a = Prompt("Введите число ");
+int CountDigit(int a)
+{
+    int count = 0; 
+    while (a > 0)
+    {
+        a = a / 10;
+        count++;
+    }
+    return count;
+}
+System.Console.WriteLine($"Цифр в числе {a} - {CountDigit(a)}");
